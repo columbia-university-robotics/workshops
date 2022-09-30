c4c26d5183db8e4241bb0da8d321c23e72de2a11
Justin Cange <jc5525@columbia.edu>
2022-09-29 23:10:01 -0400

    undirected graph

--- problems/graph.py
+++ problems/graph.py
@@ -1,15 +1,15 @@
 if __name__ == "__main__":
     graph = {}
     graph[0] = [1,2]
-    graph[1] = [0,2,7]
-    graph[2] = [0,1,3,7,9]
-    graph[3] = [2,4,6]
-    graph[4] = [3]
-    graph[6] = [3]
-    graph[7] = [1,2,8,9]
-    graph[8] = [7,10]
-    graph[10] = [8,12]
-    graph[12] = [10]
+    graph[1] = [2,7]
+    graph[2] = [1,3,7,9]
+    graph[3] = [4,6]
+    graph[4] = []
+    graph[6] = []
+    graph[7] = [8,9]
+    graph[8] = [10]
+    graph[10] = [12]
+    graph[12] = []
 
 #start at 0, return a path to 4
 for root in graph.keys():
